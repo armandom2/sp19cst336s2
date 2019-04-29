41fc5a4d6efbabee1d05967c7b50ef88defc4a42
Armando Miguel <armiguel@csumb.edu>
2019-04-29 19:08:43 +0000

    p10

--- Examples/curl/index.php
+++ Examples/curl/index.php
@@ -1,20 +1,35 @@
-<?php
-//step1
-$cSession = curl_init();
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Lab 8</title>
+        <link href="css/style.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <div id ="head">
+            <form action="/action_page.php">
+                Search: <input type="text" name="searchBox" id="userSearch"> 
+                <button id="searchButton" type="button">Search</button>
+            </form>
+            <button id="favoriteButton" type="button" onclick="location.href = 'favorites.php';">Favorites</button>
+        </div>
+        
+        <center><table id = "pictures"></table></center>
+        
+    </body>
+    
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script>
 
-//step2
-curl_setopt($cSession,CURLOPT_URL,"http://www.google.com/search?q=curl");
-curl_setopt($cSession,CURLOPT_RETURNTRANSFER,true);
-curl_setopt($cSession,CURLOPT_HEADER, false);
-
-//step3
-$results = curl_exec($cSession);
-$err = curl_error($cSession);
-
-//step4
-curl_close($cSession);
-
-//step5
-echo $results;
-
-?>
+    $(document).ready(function(){
+        $.ajax({
+            type:"GET",
+            url:"fortnite.php",
+            dataType:"json",
+            success:function(data,status){
+                console.log(data);
+            }
+        });
+    });
+    
+    </script>
+</html>
